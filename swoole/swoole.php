9475d6479c97ffe83b45480215b593d2b7b4552b
wanfeiyy <w454852895@163.com>
2016-03-28 23:26:48 +0800

    连接swoole

---
+++ swoole/swoole.php
@@ -0,0 +1,38 @@
+<?php
+class Server{
+    private  $serv;
+
+    public function __construct(){
+        $this->serv=new swoole_server('0.0.0.0',9501);
+        $this->serv->set(array(
+                'worker_num'=>8,
+                'daemonize' => false,
+                'max_request'=>1000,
+                'dispatch_mode'=>2,
+                'debug_mod'=>1,
+        ));
+        $this->serv->on('Start', array($this, 'onStart'));
+        $this->serv->on('Connect', array($this, 'onConnect'));
+        $this->serv->on('Receive', array($this, 'onReceive'));
+        $this->serv->on('Close', array($this, 'onClose'));
+        $this->serv->start();
+    }
+
+    public function onStart($serv){
+        echo "Start\r\n";
+    }
+    public function onConnect( $serv, $fd, $from_id ) {
+        $serv->send( $fd, "Hello {$fd}!" );
+    }
+
+    public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
+        echo "Get Message From Client {$fd}:{$data}\n";
+    }
+    public function onClose( $serv, $fd, $from_id ) {
+        echo "Client {$fd} close connection\n";
+    }
+
+
+}
+
+$server=new Server();
